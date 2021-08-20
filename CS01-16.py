acf5974c726838219c43c0032d2f3087727f13f4
Hana-42 <85482676+Hana-42@users.noreply.github.com>
2021-08-20 09:35:22 +0700

    Create CS01-16.py
    PS C:\Users\NITRO 5\Documents\GitHub\Hana-CS-01> & "C:/Users/NITRO 5/anaconda3/python.exe" "c:/Users/NITRO 5/Documents/GitHub/Hana-CS-01/CS01-15.py"
      File "c:/Users/NITRO 5/Documents/GitHub/Hana-CS-01/CS01-15.py", line 9
          ^
    SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
    PS C:\Users\NITRO 5\Documents\GitHub\Hana-CS-01> & "C:/Users/NITRO 5/anaconda3/python.exe" "c:/Users/NITRO 5/Documents/GitHub/Hana-CS-01/CS01-15.py"
      File "c:/Users/NITRO 5/Documents/GitHub/Hana-CS-01/CS01-15.py", line 9
        [09:21]
          ^
    SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers

---
+++ CS01-16.py
@@ -0,0 +1,20 @@
+lis=[5,10,15,20,25,50,20]
+def my_list(lis):
+    i = 0
+    for i in range (len(lis)):
+        if lis[i]==20:
+            lis[i]=200
+    print(lis)
+my_list(lis)
+[09:21]
+too easy
+[09:21]
+def my_list(lis):
+    i = 0
+    for i in range (len(lis)):
+        if lis[i]==20:
+            lis[i]=200
+    print(lis)
+lis=[5,10,15,20,25,50,20]
+my_list(lis)
+[09:21]
