5a12ef101552bf219874e4296b383aaba08bbe8b
Hana-42 <85482676+Hana-42@users.noreply.github.com>
2021-09-17 09:54:59 +0700

    Update CS01-17.py

--- CS01-17.py
+++ CS01-17.py
@@ -1,4 +1,21 @@
 import numpy as np
-from numpy import random
-arr= random.randint(100,size=(5,10))
-print(np.sort(arr))
+arry=[]
+array=[]
+m=int (input("enter the height you want for ur matrix to be "))
+n=int (input("enter the width you want for ur matrix to be "))
+x=m*n
+print ("enter number for your first array")
+for i in range(x):
+    arry+=[int(input(""))]
+NewAray = np.asarray(arry)
+print ("enter number for your second array")
+for j in range(x):
+    array+=[int(input(""))]
+NewAray = np.asarray(array)
+NewArray = np.asarray(arry)
+NewArrayNumpy = NewArray.reshape(int(m),n)
+print(NewArrayNumpy)
+NewArryNumpy = NewAray.reshape(int(n),m)
+print(NewArryNumpy)
+y=np.add(NewArrayNumpy,NewArryNumpy)
+print(y)
